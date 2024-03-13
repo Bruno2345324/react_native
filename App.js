ac2655f7acd5c47c5ae5202eb140bfddee1f320d
Bruno_@20 <159494752+Bruno2345324@users.noreply.github.com>
2024-03-13 20:12:25 -0300

    Aula 13.03

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 // import Props from './src/ex_2';
 // import Ativ_1 from './src/ativ1';
-import Exemplo_3 from './src/ex_3';
+// import Exemplo_3 from './src/ex_3';
+import Ativ_3 from './src/ativ3';
 
 
 export default function App() {
@@ -18,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 8,
