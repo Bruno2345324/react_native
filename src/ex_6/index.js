624c86d8b74681c76c2ed1fe835b6fb5715ba941
Bruno_@20 <159494752+Bruno2345324@users.noreply.github.com>
2024-04-17 20:39:34 -0300

    aula 17.04

--- src/ex_6/index.js
+++ src/ex_6/index.js
@@ -1,9 +1,67 @@
-import { View, Text, TextInput, Pressable } from 'react-native';
+import { useState } from 'react';
+import { Text, View, TextInput, Pressable } from 'react-native';
 
-export default function Exemplo_6 () {
-    return (
-        <View>
-            <Text>Exemplo_6</Text>
-        </View>
-    );
+import styles from './styles';
+
+export default function Exemplo_6() {
+
+  const [massa, setMassa] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [resultado, setResultado] = useState(0);
+  const [informacao, setInformacao] = useState('');
+
+  function Calcular() {
+    const valor = massa / (altura * altura);
+    setResultado(valor);
+  
+    if (valor < 18.5) {
+        setInformacao("Abaixo do Peso")
+    }
+    else if(valor >= 18.5 && valor < 24.9) {
+        setInformacao("Peso normal")
+    }
+    else if(valor >= 25 && valor < 29.9) {
+        setInformacao("Sobrepeso")
+    }
+    else if(valor >= 30 && valor < 34.9) {
+        setInformacao("Obesidade Grau I")
+    }
+    else if(valor >= 35 && valor < 39.9) {
+        setInformacao("Obesidade Grau II")
+    }
+    else if(valor > 40) {
+        setInformacao("Obesidade Grau III")
+    }
+}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}> Exemplo 6 </Text>
+      <View style={styles.entradaImc}>
+        <TextInput 
+          placeholder='Massa' 
+          placeholderTextColor='lightgray' 
+          keyboardType='numeric' 
+          style={styles.input} 
+          onChangeText={(entrada) => setMassa(entrada)}
+        />
+        <TextInput 
+          placeholder='Altura' 
+          placeholderTextColor='lightgray' 
+          keyboardType='numeric' 
+          style={styles.input} 
+          onChangeText={(entrada) => setAltura(entrada)}
+        />
+      </View> 
+      <Pressable style={styles.button} onPress={() => Calcular()} >
+        <Text style={styles.buttonText}> Calcular </Text>
+      </Pressable> 
+      <Text style={styles.resultados}>{resultado.toFixed(2)}</Text>
+
+      <Text style={styles.informacaoText}>{informacao}</Text>
+    </View> 
+    
+       
+    
+
+  );
 }
